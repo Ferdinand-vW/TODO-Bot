4ee8abeae9c22b244e459d0451b9930f60caaae4
Ferdinand <ferdinandvwalree@gmail.com>
2016-03-01 21:33:06 +0100

    Updated script for building on windows. Changed Output folder to bin folder

--- build-windows.bat
+++ build-windows.bat
@@ -1,1 +1,1 @@
-C:/Windows/Microsoft.NET/Framework/v4.0.30319/csc.exe /out:bin/Program.exe /recurse:src/Program.cs
+C:/Windows/Microsoft.NET/Framework/v4.0.30319/csc.exe /out:bin/Program.exe /recurse:src\*.cs
